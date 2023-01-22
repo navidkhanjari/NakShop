a8bd8e09fb027d1ff06a9dcd5d8d4f70d056e05d
navidkhanjari72 <navidkhanjari72@gmail.com>
2023-01-22 22:44:47 +0330

    Add Product Aggregate

---
+++ src/Shop/Shop.Domain/ProductAgg/ProductSpecification.cs
@@ -0,0 +1,22 @@
+﻿using Common.Domain;
+using Common.Domain.Exceptions;
+using System;
+
+namespace Shop.Domain.ProductAgg
+{
+    public class ProductSpecification : BaseEntity
+    {
+        public ProductSpecification(string key, string value)
+        {
+            NullOrEmptyDomainDataException.CheckString(key, nameof(key));
+            NullOrEmptyDomainDataException.CheckString(value, nameof(value));
+
+            Key = key;
+            Value = value;
+        }
+
+        public Guid ProductId { get; internal set; }
+        public string Key { get; private set; }
+        public string Value { get; private set; }
+    }
+}
