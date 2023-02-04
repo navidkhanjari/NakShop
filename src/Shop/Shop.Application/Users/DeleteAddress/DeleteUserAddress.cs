c9b87b372196b3f1a65e2cd74fb209b0a5ad038f
navidkhanjari72 <navidkhanjari72@gmail.com>
2023-02-04 17:30:44 +0330

    Complete commands

---
+++ src/Shop/Shop.Application/Users/DeleteAddress/DeleteUserAddress.cs
@@ -0,0 +1,7 @@
+﻿using Common.Application;
+using System;
+
+namespace Shop.Application.Users.DeleteAddress
+{
+    public record DeleteUserAddressCommand(Guid UserId, Guid AddressId) : IBaseCommand;
+}
---
+++ src/Shop/Shop.Application/Users/Register/RegisterUserCommand.cs
@@ -0,0 +1,20 @@
+﻿using Common.Application;
+using Common.Domain.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Shop.Application.Users.Register
+{
+    public class RegisterUserCommand : IBaseCommand
+    {
+        public RegisterUserCommand(PhoneNumber phoneNumber, string password)
+        {
+            PhoneNumber = phoneNumber;
+            Password = password;
+        }
+        public PhoneNumber PhoneNumber { get; private set; }
+        public string Password { get; private set; }
+    }
+}
